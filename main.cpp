bdad0b1c1f9597e52ca0aa3faf95f423ef254111
T04-ka <sushkov.infor.548@yandex.ru>
2026-08-20 21:11:51 +0300

    Сделал строчный ввод в виде функции

---
+++ main.cpp
@@ -0,0 +1,159 @@
+#include "./init.h"
+
+#include <ctype.h>
+
+
+Bool is_OnlySpace_in_line(char *s);
+Bool is_OnlyDigit_in_line(char *s);
+int get_line(char *s, int maxlen);
+Nroots squareSolve(const double *coefs, double *roots);
+void printRes(const double *roots, enum Nroots nroots);
+void readParam(const char type_of_param, double *param);
+
+const char types[] = {'A','B','C'};
+
+int main(){
+      
+      //initialization
+      double coefs[] = {0, 0, 0}; // A B C
+      double roots[] = {0, 0}; // x1 x2
+      
+      
+      printf("Enter data: \"A B C\" for equation A*x^2 + B*x + c = 0.");
+      
+      int i = 0;
+      for (i = 0; i < 3; i++){
+            readParam(types[i], coefs+i);
+      }
+      
+      
+      Nroots nroots =  squareSolve(coefs, roots);
+      
+      printRes(roots, nroots);
+        
+      return 0;   
+}
+
+//------------------------------------------------
+
+void readParam(const char type_param, double *param){
+      
+      char line[MAXLEN] = {};
+      int f = 0, input=0;
+      Bool err = NO;
+      
+      
+      do {  
+            printf("\nPlease, enter parametr %c: ", type_param);  
+            err = NO;
+            get_line(line, MAXLEN);            
+            input = sscanf(line, "%lg", param);
+            
+            if (is_OnlySpace_in_line(line) == YES) {
+                        
+                  printRes({0}, EMPTY_INPUT);
+                  err = YES;
+            }
+            
+            else if (is_OnlyDigit_in_line(line) == NO){
+                  
+                  printRes({0}, INPUT_ERROR);
+                  err = YES;
+            }
+      
+      } while (!(input == 1 && err == NO));
+          
+}
+
+//--------------------------------
+
+Bool is_OnlySpace_in_line(char *s){
+
+      int i = 0;
+      char c = 0;
+      while ((c = s[i++]) != '\n' && c != '\0'){
+      
+            if (!isspace(c)) return NO;
+      }
+      
+      return YES;
+}
+
+//--------------------
+
+Bool is_OnlyDigit_in_line(char *s){
+      
+      int i = 0;
+      char c = 0;
+      while ((c=s[i++]) != '\n' && c != '\0'){
+            
+            if (!isdigit(c)) return NO;
+      }
+      
+      return YES;
+}
+
+//---------------------------------------------------------
+
+int get_line(char *s, int mxlen){
+      
+      int c = 0;
+      int len = 0;
+      
+      while ((c=getchar()) != '\n' && c != EOF && len < mxlen){
+            
+            s[len++] = (char) c;      
+      }
+      
+      if (c == '\n') {
+      
+            s[len++] = (char) c;
+      }
+      
+      s[len] = '\0';
+      
+      return len;      
+}
+
+//------------------------------------------------------------
+
+void printRes(const double *roots, Nroots nroots){
+      
+      switch (nroots) {
+      
+          case ZERO_ROOT: {
+                printf("\nEquation has no roots.\n");
+                break;
+          }
+          
+          case ONE_ROOT: {
+                printf("Equation has 1 root: x = %g.\n", roots[0]);
+                break;
+          }
+          
+          case TWO_ROOT: {
+                printf("Equation has 2 roots: x1 = %g, x2 = %g.\n", roots[0], roots[1]);
+                break;
+          }
+          
+          case INF_ROOT: {
+                printf("Equation has infinity roots.\n");
+                break;
+          }
+          
+          case EMPTY_INPUT: {
+                printf("Error: empty input.\n");
+                break;
+          }
+          
+          case INPUT_ERROR: {
+                printf("\nError: wrong input.\n");
+                break;
+          }
+          
+          default: {
+                
+                printf("Unknown error.\n");
+          }
+      }
+}
---
+++ sqSolve.cpp
@@ -0,0 +1,61 @@
+#include "./init.h"
+
+Bool is_zero(double x);
+Nroots squareSolve(const double *coefs, double *roots);
+
+#define ZERO_BORDER 0.000001
+
+Nroots squareSolve(const double *coefs, double *roots){
+      double a = *coefs, b = *(coefs+1), c = *(coefs+2);
+      if (is_zero(a)){ //A=0
+      
+            if (is_zero(b)){ //B=0
+                        
+                  return (is_zero(c)) ? INF_ROOT : ZERO_ROOT; //A=0 B=0 C?=0
+            }
+            else { //A=0 B!=0 C?=0
+                  
+                  *roots = -c/b;
+                  return ONE_ROOT;
+            }
+      }
+      else { //A!=0 B?=0 C?=0
+            
+            double D = b * b - 4.0 * a * c;
+            
+            if (is_zero(D)){
+            
+                  *roots = -b / (2.0*a);
+                  
+                  return ONE_ROOT;
+            }
+            else if (D < 0) {
+                  
+                  return ZERO_ROOT;
+            }
+            else {
+            
+                  *roots = (-b - sqrt(D)) / (2.0 * a); //x1
+                  *(roots+1) = (-b + sqrt(D)) / (2.0 * a); //x2
+                  
+                  return TWO_ROOT;
+            }
+      }
+}
+
+
+Bool is_zero(double x){
+      
+      if (x > 0){
+            
+            return (x < ZERO_BORDER) ? YES
+                            : NO;
+      }
+      
+      else{
+      
+            return (x > -ZERO_BORDER) ? YES
+                             : NO;
+      }
+}
+
